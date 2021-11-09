9e90a304441d38df8325e682a8081bc0e47b0366
Noemie Brancourt <noemie_brancourt@hotmail.com>
2021-11-09 15:10:02 +0100

    added links to new task, edit task & back to all tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,6 +18,17 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    # find the task we're updating
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
