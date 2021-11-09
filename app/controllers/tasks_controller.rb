ba88c3f69fd84ebcd06516ee3fc73b4077195bce
Noemie Brancourt <noemie_brancourt@hotmail.com>
2021-11-09 14:29:22 +0100

    Show individual task done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,10 @@ class TasksController < ApplicationController
     # get all the restaurants from the db
     @tasks = Task.all
   end
+
+  def show
+    # we need to get the id of the restaurant
+    # go to the db and get the restaurant instance with that id
+    @task = Task.find(params[:id])
+  end
 end
