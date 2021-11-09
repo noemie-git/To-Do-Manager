e760aec5e540dfd11772cc91e107be9d95a714b4
Noemie Brancourt <noemie_brancourt@hotmail.com>
2021-11-09 16:36:34 +0100

    checkboxes WIP

--- db/migrate/20211109112417_change_completed_in_tasks.rb
+++ db/migrate/20211109112417_change_completed_in_tasks.rb
@@ -1,5 +1,5 @@
 class ChangeCompletedInTasks < ActiveRecord::Migration[6.0]
   def change
-    change_column :tasks, :completed, :boolean, default: false
+    change_column :tasks, :completed, :boolean, default: true
   end
 end
